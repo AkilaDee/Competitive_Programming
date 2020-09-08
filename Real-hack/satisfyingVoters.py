33475f09dac8cffaa34b91251b381dbac53a1e91
Akila De Silva <54980465+AkilaDee@users.noreply.github.com>
2020-09-08 22:17:09 +0530

    Create satisfyingVoters.py

---
+++ Real-hack/satisfyingVoters.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    n1 = list(map(int, input().split()))
+    n2 = list(map(int, input().split()))
+    n2.sort()
+    
+    sumO = 0
+    voters = 0
+    
+    for i in range (0, n1[0]):
+        if(n1[1] < sumO):
+            break
+        else:
+            sumO = sumO + n2[i]
+            voters = voters+1
+            
+    print (voters-1)
